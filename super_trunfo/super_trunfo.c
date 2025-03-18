55897dd81b2b9a560080ac3775c73c34b7bfde8e
Edison de Almeida <edi_almeida2011@hotmail.com>
2025-03-18 14:42:00 +0000

    primeiro commit

---
+++ super_trunfo/super_trunfo.c
@@ -0,0 +1,39 @@
+// Cdastrando duas cartas Super Trunfo
+#include <stdio.h>
+
+int main(){
+
+    // variáveis para armazenar os dados
+    char estado;
+    char codigo_carta[3];
+    char cidade[20];
+    int populacao, pontos_turisticos;
+    float area, pib;
+    
+    printf("--- Vamos começar inserindo os dados de cada carta --- \n");
+    printf("Digite a primeira letra do nome do Estado: \n");
+    scanf("%c", &estado);
+    printf("Código da carta(A letra do estado seguido de um número ex: S01, A02, B03): \n");
+    scanf("%s", &codigo_carta);
+    printf("Nome da Cidade: \n");
+    scanf("%s", cidade);
+    printf("População: \n");
+    scanf("%i", &populacao);
+    printf("Área: km² \n");
+    scanf("%f", &area);
+    printf("PIB: R$ \n");
+    scanf("%f", &pib);
+    printf("Pontos Turísticos: \n");
+    scanf("%f", &pontos_turisticos);
+
+
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo_carta);
+    printf("Nome da cidade: %s\n", cidade);
+    printf("População: %i\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    return 0;
+
+}
